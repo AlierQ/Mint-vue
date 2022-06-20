aa66a34c4c42513819fc8c5bc8800c704436f4c2
AlierQ <1029369938@qq.com>
2022-06-20 18:23:21 +0800

    add 404 page

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Detail from "@/views/Detail.vue";
 import Add from "@/views/Add.vue";
 import Statistics from "@/views/Statistics.vue";
 import Label from "@/views/Label.vue";
+import NotFound from "@/views/NotFound.vue";
 
 Vue.use(VueRouter);
 
@@ -25,6 +26,12 @@ const routes: Array<RouteConfig> = [
   {
     path: "/label",
     component: Label,
+  },
+  // 前面的都没有匹配到才会匹配下面的
+  {
+    // 除了上面之外的所有路径都是404
+    path: "*",
+    component: NotFound,
   },
 ];
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      <!-- <a href="#/">返回首页</a> -->
+      <router-link to="/">点击返回首页</router-link>
+    </div>
+    <div>请求资源不存在，请检查网址是否正确！</div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style lang="scss" scoped></style>
