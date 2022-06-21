aa5b914f2f457e2acb6132e4b3bbf9018b2dfed8
AlierQ <1029369938@qq.com>
2022-06-21 22:15:50 +0800

    使用svgo-loader去除svg默认fill(样式)

--- vue.config.js
+++ vue.config.js
@@ -8,11 +8,19 @@ module.exports = {
     config.module
       .rule("svg-sprite")
       .test(/\.svg$/) // 匹配正则
-      .include.add(dir) // 只让dir下的svg走这个规则
-      .end()
+      .include.add(dir)
+      .end() // 只让dir下的svg走这个规则
       .use("svg-sprite-loader")
       .loader("svg-sprite-loader")
       .options({ extract: false }) // 不解析出文件
+      .end()
+      // 去除svg默认带有的fill属性
+      .use("svgo-loader")
+      .loader("svgo-loader")
+      .tap((options) => ({
+        ...options,
+        plugins: [{ removeAttrs: { attrs: "fill" } }],
+      }))
       .end();
     config
       .plugin("svg-sprite")
