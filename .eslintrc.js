b1ff46e396adc47e2093bf5fd74405a1fe10942c
AlierQ <1029369938@qq.com>
2022-06-23 17:15:24 +0800

    解决报错@typescript-eslint/explicit-module-boundary-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "@typescript-eslint/no-var-requires": 0,
+    "@typescript-eslint/explicit-module-boundary-types": "off",
   },
   globals: {
     __WebpackModuleApi: "writable",
