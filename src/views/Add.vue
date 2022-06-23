c4b9940979042a8cff00ad41222bf7e28aa9ceab
AlierQ <1029369938@qq.com>
2022-06-23 16:51:16 +0800

    使用js编写组件

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -1,7 +1,7 @@
 /**
- * @Description: 
+ * @Description:
  * @author AlierQ
- * @date 20/6/2022 
+ * @date 20/6/2022
 */
 <template>
   <div>
@@ -10,8 +10,17 @@
       <!-- 通过slot属性指定某一个插槽 -->
       <template slot="top">
         <div class="top">
-          <div class="out selected">支出</div>
-          <div class="in">收入</div>
+          <!-- 已有基础样式元素，添加样式的方式，更推荐后一种，多个class属性会合并 -->
+          <div
+            :class="type === '-' ? 'out selected' : 'out'"
+            @click="selectType('-')">支出
+          </div>
+          <!-- type === '+'为false默认会被忽略 -->
+          <div
+            class="in"
+            :class="type === '+' && 'selected'"
+            @click="selectType('+')">收入
+          </div>
           <div class="close">取消</div>
         </div>
       </template>
@@ -23,39 +32,52 @@
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import InputPad from "@/components/InputPad.vue";
 export default {
   name: "ADD",
   components: { InputPad },
+  data() {
+    return {
+      type: "-", // '-' 表示支出 '+' 表示收入
+    };
+  },
+  methods: {
+    selectType(type) {
+      if (type === "-" || type === "+") this.type = type;
+      else throw new Error("type is unknown");
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
-.top{
+.top {
   height: 80px;
   background: $color-basic;
   display: flex;
   justify-content: center;
   align-items: flex-end;
   padding-bottom: 10px;
-  .in,.out{
+  .in,
+  .out {
     font-size: 22px;
     margin: 0 10px;
     position: relative;
-    &.selected{
-      &::after{
+    &.selected {
+      &::after {
         content: "";
-        position:absolute;
-        left: 0;bottom: 0;
+        position: absolute;
+        left: 0;
+        bottom: 0;
         width: 100%;
         height: 2px;
         background: #333;
       }
     }
   }
-  .close{
+  .close {
     position: absolute;
     right: 20px;
     font-size: 18px;
