e816f0efa8fa35721a6d76a172b3cf4721d28c97
AlierQ <1029369938@qq.com>
2022-06-23 15:56:12 +0800

    编写add页面的顶部样式

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -8,12 +8,14 @@
     <Layout>
       <!-- 调用子组件并且将内容插到子元素内部的插槽中 -->
       <!-- 通过slot属性指定某一个插槽 -->
-      <template slot="content">
+      <template slot="top">
         <div class="top">
-
+          <div class="out selected">支出</div>
+          <div class="in">收入</div>
+          <div class="close">取消</div>
         </div>
       </template>
-      <template slot="content">add.vue</template>
+      <template slot="content">这里是内容</template>
       <template slot="bottom">
         <InputPad></InputPad>
       </template>
@@ -29,4 +31,34 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+.top{
+  height: 80px;
+  background: $color-basic;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  padding-bottom: 10px;
+  .in,.out{
+    font-size: 22px;
+    margin: 0 10px;
+    position: relative;
+    &.selected{
+      &::after{
+        content: "";
+        position:absolute;
+        left: 0;bottom: 0;
+        width: 100%;
+        height: 2px;
+        background: #333;
+      }
+    }
+  }
+  .close{
+    position: absolute;
+    right: 20px;
+    font-size: 18px;
+  }
+}
+</style>
